b218e90e0272f79ce29441dedd9b9a9cb592058b
wishabc <aswq22013@gmail.com>
2024-03-07 18:26:50 -0800

    Fix duplicate positions bug in convert_to_coo.py

--- bin/convert_to_coo.py
+++ bin/convert_to_coo.py
@@ -140,7 +140,7 @@ def create_coo_from_bed(bed_df, genome_length):
         fwd = 1 if row['strand'] == '+' else -1
         for j, mo in enumerate(['ref_m6a', 'ref_5mC'], 1):
             mo_pos = np.array([int(x) for x in row[mo].strip(',').split(',') if x not in incorrect_positions]) - row['start']
-            if mo_pos.size != np.unique(mo_pos).size:
+            if mo_pos.size != np.unique(mo_pos).size & j == 1:
                 print(row['fiber'])
                 unique, counts = np.unique(mo_pos, return_counts=True)
                 repetitive_numbers = unique[counts > 1]
